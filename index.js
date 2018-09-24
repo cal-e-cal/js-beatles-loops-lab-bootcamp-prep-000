30629ece7138d7e0b07b290489931ad7f2fb811b
cyu8110 <cyu8110@gmail.com>
2018-09-24 20:07:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay (musicians, instruments) {
   var newArray = [];
   for (let i = 0; i < musicians.length ; i++) {
-    return newArray.push (`musicians${[i]} plays instruments${[i]}`);
+    return newArray.push (musicians[i] + " plays " + instruments[i]);
   }
 }
